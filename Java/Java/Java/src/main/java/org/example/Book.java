f024e3f90a16b70c926fc091d121f1297f384be7
Viktoria Polyakovich <polyakovichvika@yandex.by>
2023-09-24 15:05:43 +0300

    Homework5

--- Java/Java/Java/src/main/java/org/example/Book.java
+++ Java/Java/Java/src/main/java/org/example/Book.java
@@ -4,34 +4,41 @@ import java.util.*;
 
 public class Book {
     public static void main(String[] args) {
-        HashMap<String, String> phonebook = new HashMap<>();
+        HashMap<String, ArrayList> phonebook = new HashMap<>();
         List<String> names = Arrays.asList("Алена", "Виктор","Федор", "Геннадий", "Петр", "Василий", "Виктор", "Петр");
-        List<String> numbers = Arrays.asList("5454622", "5232500","5111107", "5454689", "5432231", "1234520", "8654233", "5962352");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Collections.addAll(numbers,5454622, 8654233, 5111107, 5454689, 5432231, 1234520, 5232500, 5962352);
 
+
+        //добавление контактов
 
         for (int i = 0; i < names.size(); i++) {
+            ArrayList templist = new ArrayList<>();
             if (phonebook.containsKey(names.get(i))) {
-                for (String k:names) {
-                    if (names.get(i) == k) {
-                        String value = String.valueOf(phonebook.get(k) + ", " +numbers.get(i));
-                        phonebook.put(k, value);
-                        break;
+                for (int j = 0; j < names.size(); j++) {
+                    if (names.get(j) == names.get(i)){
+                        templist.add(numbers.get(j));
                     }
                 }
-
             } else {
-                phonebook.put(names.get(i), numbers.get(i));
+                templist.add(numbers.get(i));
             }
-
+            phonebook.put(names.get(i), templist);
         }
 
-        LinkedHashMap<String, String> sorted_book = new LinkedHashMap<>();
+
+        //сортировка контактов
+        HashMap<String, ArrayList> sorted_book = new LinkedHashMap<>(phonebook);
         phonebook.entrySet().stream()
-                        .sorted(Map.Entry.comparingByValue(Comparator.comparingInt(e -> Integer.parseInt(e))))
-                        .filter(e->e.getValue().length()>7)
+                        .sorted(Map.Entry.comparingByValue(Comparator.comparingInt(e -> (int) e.get(0))))
                         .forEach(e->sorted_book.put(e.getKey(), e.getValue()));
 
 
+
+
+
+
         System.out.println(phonebook);
+        System.out.println(sorted_book);
     }
 }
